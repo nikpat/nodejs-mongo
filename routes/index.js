6dcf05e6a024c22e859dc055d087041ec8ee43a9
nick <nick@nick.(none)>
2013-03-07 22:03:44 +0530

    "login"

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,56 @@
 /*
  * GET home page.
  */
-var Db = require('mongodb').Db;
-var Server = require('mongodb').Server;
-var client = new Db('test', new Server('127.0.0.1', 27017, {}));
+var mongoose = require('mongoose');
+mongoose.connect('localhost', 'test');
+
+var usrStruc = mongoose.Schema({
+    	username: String,
+    	password:String
+	});
+
+var userObj = mongoose.model('user', usrStruc);
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	var logerr;
+	//express 3.1 does not have flash therefore do folloing step
+	if(req.session.logerr != undefined){
+		logerr = req.session.logerr;
+		delete req.session.logerr ;
+	}
+  res.render('index', { title: 'Express ToDo',errLog : logerr });
 };
 
 exports.dashboard = function(req,res){
-	//res.send('hello');
-	var uname = req.query.uname;
-	client.open(function(err, pClient) {
-			client.collection('test',function(err,c){
-				c.insert({name: uname})
-			});
-			client.close();
-	});
+	var userArr = [];
+	var uname = req.body.uname;
+	var upass = req.body.upass;
+	//check if the user exists in mongodb
+	var query = userObj.findOne({ username: uname,password:upass });
 
-	res.render('dash',{title:'Dashboard',name:uname+' len='+uname.length});
+	query.exec(function(err,user){
+		if (err) return handleError(err);
+		if(user){
+			res.render('dash',{title:'Dashboard',name:uname});
+		}
+		else{
+			 //Set session
+			 req.session.logerr = "User Not Found !"
+   			 res.redirect('/');
+		}
+	});
+	/*
+	var user = new userObj({ username: uname,password:'test' });
+	//save user to database
+
+	user.save(function (err) {
+  		console.log('saved');
+	});
+	///finduser user to database
+	usersdata = userObj.find(function(err,users){
+			for(i=0;i<users.length;i++){
+				userArr.push(users[i]);
+			}
+			res.render('dash',{title:'Dashboard',name:uname,users:userArr});
+		}); */
 };
