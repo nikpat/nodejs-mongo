6c7159ad8245d9339f7a65046d1e104f1082d69d
Ashutosh <prathameshnavghare@Ashus-Mac-mini.local>
2013-03-12 21:28:08 +0530

    login redirection

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.post('/dash/', routes.dashboard);
 app.get('/users/', user.list);
+app.get('/logout', routes.logout);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -14,24 +14,41 @@ var userObj = mongoose.model('user', usrStruc);
 
 exports.index = function(req, res){
 	var logerr;
-	//express 3.1 does not have flash therefore do folloing step
-	if(req.session.logerr != undefined){
-		logerr = req.session.logerr;
-		delete req.session.logerr ;
+	if(req.session.user != undefined){
+		if(req.session.user.username != 'admin'){
+			res.redirect('/dash');
+		}
+		else{
+			res.redirect('/dash');	
+		}
 	}
-  res.render('index', { title: 'Express ToDo',errLog : logerr });
+	else{
+	//express 3.1 does not have flash therefore do folloing step
+		if(req.session.logerr != undefined){
+			logerr = req.session.logerr;
+			delete req.session.logerr ;
+		}
+  		res.render('index', { title: 'Express ToDo',errLog : logerr });
+	}
 };
 
 exports.dashboard = function(req,res){
 	var userArr = [];
-	var uname = req.body.uname;
-	var upass = req.body.upass;
+	if(req.session.user == undefined){
+		var uname = req.body.uname;
+		var upass = req.body.upass;
+	}
+	else{
+		var uname = req.session.user.username;
+		var upass = req.session.user.password;
+	}
 	//check if the user exists in mongodb
+	
 	var query = userObj.findOne({ username: uname,password:upass });
-
 	query.exec(function(err,user){
 		if (err) return handleError(err);
 		if(user){
+			req.session.user = user;
 			res.render('dash',{title:'Dashboard',name:uname});
 		}
 		else{
@@ -55,3 +72,8 @@ exports.dashboard = function(req,res){
 			res.render('dash',{title:'Dashboard',name:uname,users:userArr});
 		}); */
 };
+
+exports.logout = function(req,res){
+	delete res.session;
+	res.redirect('/');
+}
